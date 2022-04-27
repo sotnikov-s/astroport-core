73580a531cc728c169e111b2c8d9e82ebc73bcec
sotnikov-s <ssotnikov93@gmail.com>
2022-04-27 14:51:48 +0200

    add fixed rate provider schema

---
+++ contracts/fixed_rate_provider/examples/fixed_rate_provider_schema.rs
@@ -0,0 +1,23 @@
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+use cosmwasm_schema::{export_schema_with_title, remove_schemas, schema_for};
+
+use astroport::fixed_rate_provider::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use astroport::rate_provider::GetExchangeRateResponse;
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema_with_title(&schema_for!(InstantiateMsg), &out_dir, "InstantiateMsg");
+    export_schema_with_title(&schema_for!(ExecuteMsg), &out_dir, "ExecuteMsg");
+    export_schema_with_title(&schema_for!(QueryMsg), &out_dir, "QueryMsg");
+    export_schema_with_title(
+        &schema_for!(GetExchangeRateResponse),
+        &out_dir,
+        "GetExchangeRate",
+    );
+}
